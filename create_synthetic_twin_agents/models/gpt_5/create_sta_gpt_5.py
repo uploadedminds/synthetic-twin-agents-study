d94e20a772dcb4578f99749345daaf12016fbe12
asdasd <julian.ustiyanovych@gmail.com>
2025-08-17 12:16:03 +0200

    add new models

---
+++ create_synthetic_twin_agents/models/gpt_5/create_sta_gpt_5.py
@@ -0,0 +1,59 @@
+import os
+from edsl import ScenarioList, AgentList, QuestionLinearScale, Model
+from dotenv import load_dotenv
+
+load_dotenv()  # By default, loads from .env in the current directory
+
+try:
+    sl = ScenarioList.pull("ddfc9685-f065-4f06-b22f-7ed5a3a691bb")
+    agents = AgentList.pull("f33e3099-2757-4ac4-a626-03d949adb912")
+except Exception as e:
+    print(f"Error pulling scenario or agents: {e}")
+    sl = ScenarioList.pull("ddfc9685-f065-4f06-b22f-7ed5a3a691bb")
+    agents = AgentList.pull("f33e3099-2757-4ac4-a626-03d949adb912")
+    print(f"Pulled scenario and agents: {sl} {agents}")
+
+# (Optional) inspect what's inside
+print(f"Loaded scenario: {len(sl)}")
+print(f"Number of agents: {len(agents)}")
+
+
+
+# Create the question object
+q = QuestionLinearScale(
+    question_name="question",
+    question_text="""
+    Please evaluate the effectiveness of this product ad by indicating the extent to which you agree with the following statement: 
+    {{ statement }}.
+    
+    The ad includes three images:
+    
+    1. {{ image_1 }}
+    2. {{ image_2 }}
+    3. {{ image_3 }}
+    
+    A a title: {{ title }}, and a description: {{ description }}.
+    """,
+    question_options=[
+        1, 2, 3, 4, 5
+    ],
+    option_labels={
+        1: "Strongly disagree",
+        2: "Disagree",
+        3: "Neither agree nor disagree",
+        4: "Agree",
+        5: "Strongly agree"
+    }
+)
+
+# Create the model object
+
+m = Model("gpt5", service_name = "openai", temperature = 1)
+
+results = q.by(sl).by(agents[:5]).by(m).run(disable_remote_cache=True ,disable_remote_inference=True, progress_bar=True, verbose=True)
+
+output_path = "../../synthetics_survey_results/gpt_5/results_4.csv"
+
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+results.to_pandas().to_csv(output_path, index=False)
